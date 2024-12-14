38fd2b73ce569dc81cd4ab032d56905cef749ca2
kadirosmanust <kadirosmanust98@gmail.com>
2024-12-14 21:26:06 +0300

    feat: handle with docker build

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM node:20-alpine AS dependencies-env
-RUN npm i -g yarn
 WORKDIR /app
 COPY package.json yarn.lock ./
 RUN yarn install
@@ -18,5 +17,6 @@ RUN yarn build
 FROM node:20-alpine
 WORKDIR /app
 COPY --from=production-dependencies-env /app /app
-COPY --from=build-env /app/build /app/build
+COPY --from=build-env /app/dist /app/dist
+COPY --from=production-dependencies-env /app/node_modules /app/node_modules
 CMD ["yarn", "start"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
+/* eslint-disable no-underscore-dangle */
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
   },
 });

2d0a2283c608595572a5ee67cb7a8a6572d36a73
kadirosmanust <kadirosmanust98@gmail.com>
2024-12-14 16:41:58 +0300

    feat: add pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,28 @@
+import { Pagination as AntdPagination } from "antd";
+
+type PaginationProps = {
+  currentPage?: number;
+  totalSize?: number;
+  pageSize?: number;
+  onChange?: (page: number) => void;
+};
+
+const Pagination = ({
+  currentPage=1,
+  totalSize,
+  pageSize = 10,
+  onChange,
+}: PaginationProps) => {
+  return (
+    <AntdPagination
+      align="center"
+      current={currentPage}
+      pageSize={pageSize}
+      onChange={onChange}
+      total={totalSize}
+      showSizeChanger={false}
+    />
+  );
+};
+
+export default Pagination;

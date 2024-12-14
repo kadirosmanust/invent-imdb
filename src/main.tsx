281af95e37f9527da74a3549ef3fc0ddc3323c7c
kadirosmanust <kadirosmanust98@gmail.com>
2024-12-14 16:40:18 +0300

    feat: add redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "@/store";
+import { ConfigProvider } from "antd";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#4c1d95",
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </Provider>
+);
